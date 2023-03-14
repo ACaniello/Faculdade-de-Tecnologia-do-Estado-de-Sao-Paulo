ca0a0a33f3e6de1c3badcc9d2a0e9fa3d3c00a36
Aldo Costa Silveira <61122744+backtoaldo@users.noreply.github.com>
2023-03-14 10:44:32 -0300

    Add files via upload

---
+++ Técnicas de Programação I - ILP036/Exercicios/JavaApplication1/src/javaapplication1/Motor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Motor {
+    int potencia;
+    float capacidade;
+    int cilindros;
+    
+    public void ligar() {
+        System.out.println("Motor ligado");
+    }
+}
