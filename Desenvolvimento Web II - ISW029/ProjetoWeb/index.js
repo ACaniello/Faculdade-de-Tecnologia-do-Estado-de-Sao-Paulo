aa53fc5fa25cf818d89d6342eb7361ff998addc4
Aldo Costa Silveira <61122744+backtoaldo@users.noreply.github.com>
2023-05-10 11:21:48 -0300

    uso do firebase
    Projeto simples mostrando a funcionalidade do firebase

---
+++ Desenvolvimento Web II - ISW029/ProjetoWeb/index.js
@@ -0,0 +1,28 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('projetoweb-66eba-firebase-adminsdk-cfujl-9a4c5aa990.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+const data = {
+    name: 'Los Angeles',
+    state: 'CA',
+    country: 'USA'
+  };
+  
+  const cityRef = db.collection('cities').doc('SF');
+  const doc = await cityRef.get();
+  if (!doc.exists) {
+    console.log('No such document!');
+  } else {
+    console.log('Document data:', doc.data());
+  }
+  
+  // Add a new document in collection "cities" with ID 'LA'
+  const res = await db.collection('projetoweb').doc('LA').set(data);
+  
