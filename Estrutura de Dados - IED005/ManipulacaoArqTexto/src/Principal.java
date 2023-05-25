22a6b61c1fde90b0c6cd3b890e1eb945b038915f
Aldo Costa Silveira <61122744+backtoaldo@users.noreply.github.com>
2023-05-25 09:36:05 -0300

    Manipulação de Arquivo
    Leitura e escrita de um arquivo.

---
+++ Estrutura de Dados - IED005/ManipulacaoArqTexto/src/Principal.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Principal {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        leituraArquivo();
+        escritaArquivo();     
+    
+}
+
+    private static void leituraArquivo() throws IOException, FileNotFoundException {
+        final String ARQ_ENT = "C:\\Users\\FATEC ZONA LESTE\\Desktop\\samba_enredo.txt";
+        FileReader fr = new FileReader (ARQ_ENT);
+        BufferedReader br = new BufferedReader(fr);
+        
+        String linha = br.readLine();
+        System.out.println(linha);
+        
+        while (linha != null){
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+    }
+
+    private static void escritaArquivo() throws IOException {
+        final String ARQ_SAIDA = "C:\\Users\\FATEC ZONA LESTE\\Desktop\\arquivo_saida.txt";
+        FileWriter fw = new FileWriter (ARQ_SAIDA);
+        BufferedWriter bw = new BufferedWriter(fw);
+        String linha = "primeira linha";
+        bw.write(linha); bw.newLine();
+        
+        linha = "segunda linha";
+        bw.write(linha);
+        
+        bw.flush();
+    
+    }
+}
